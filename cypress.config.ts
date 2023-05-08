d5951a87fa51908110d91cb0703efb0d7a71e159
Pedro Rodriguez Cardona <PedroAlejandro.RodriguezCardona@marcusmillichap.com>
2023-05-08 12:03:25 -0500

    Added configuration for cocontinuous Integration

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,10 +4,13 @@ export default defineConfig({
   // setupNodeEvents can be defined in either
   // the e2e or component configuration
   e2e: {
+  video: false,
+  screenshotOnRunFailure: false,
+  chromeWebSecurity: false,
    setupNodeEvents(on, config) {
-     // modify config values examples
-     // config.defaultCommandTimeout = 10000
-     // IMPORTANT return the updated config object
+    config.defaultCommandTimeout= 10000;
+    config.responseTimeout= 20000;
+    config.pageLoadTimeout= 60000;
      return config;
     },
   },
